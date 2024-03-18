d08e728eb7485a7906cd6550d199abc84f288419
Peter Marshall <peterdmarshall99@gmail.com>
2024-03-18 00:42:03 -0400

    go fmt

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-  "github.com/apple/foundationdb/bindings/go/src/fdb"
-  "errors"
+	"errors"
+	"github.com/apple/foundationdb/bindings/go/src/fdb"
 
 	"context"
 	"fmt"
@@ -12,7 +12,7 @@ import (
 	"github.com/danielgtaylor/huma/v2/adapters/humachi"
 	"github.com/danielgtaylor/huma/v2/humacli"
 	"github.com/go-chi/chi/v5"
-  "github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 // Options for the CLI. Pass `--port` or set the `SERVICE_PORT` env var.
@@ -21,20 +21,20 @@ type Options struct {
 }
 
 type GetKeyResponse struct {
-  Body struct {
-    Key string `json:"key" example:"foo"`
-    Value string `json:"value" example:"bar"`
-  } 
+	Body struct {
+		Key   string `json:"key" example:"foo"`
+		Value string `json:"value" example:"bar"`
+	}
 }
 
 type SetKeyResponse struct {
-  Body struct {
-    Result string `json:"result" example:"ok"`
-  }
+	Body struct {
+		Result string `json:"result" example:"ok"`
+	}
 }
 
 type SetKeyRequestBody struct {
-  Value string `json:"value"`
+	Value string `json:"value"`
 }
 
 func main() {
@@ -42,50 +42,50 @@ func main() {
 	cli := humacli.New(func(hooks humacli.Hooks, options *Options) {
 		// Create a new router & API
 		router := chi.NewMux()
-    router.Use(middleware.Logger)
+		router.Use(middleware.Logger)
 		api := humachi.New(router, huma.DefaultConfig("My API", "1.0.0"))
 
-    // Different API versions may expose different runtime behaviors.
-    fdb.MustAPIVersion(710)
+		// Different API versions may expose different runtime behaviors.
+		fdb.MustAPIVersion(710)
 
-    // Open the default database from the system cluster
-    db := fdb.MustOpenDefault()	
+		// Open the default database from the system cluster
+		db := fdb.MustOpenDefault()
 
-    huma.Put(api, "/{key}", func(ctx context.Context, input *struct{
-      Key string `path:"key"`
-      Body SetKeyRequestBody `contentType:"application/json"`
-    }) (*SetKeyResponse, error) {
-      resp := &SetKeyResponse{}
-      _, err := db.Transact(func(tr fdb.Transaction) (interface{}, error) {
-        tr.Set(fdb.Key(input.Key), []byte(input.Body.Value))
-        return nil, nil
-      })
-      if err != nil {
-        resp.Body.Result = "err"
-        return resp, errors.New("failed to set key") 
-      }
-      resp.Body.Result = "ok" 
-      return resp, nil
-    })
+		huma.Put(api, "/{key}", func(ctx context.Context, input *struct {
+			Key  string            `path:"key"`
+			Body SetKeyRequestBody `contentType:"application/json"`
+		}) (*SetKeyResponse, error) {
+			resp := &SetKeyResponse{}
+			_, err := db.Transact(func(tr fdb.Transaction) (interface{}, error) {
+				tr.Set(fdb.Key(input.Key), []byte(input.Body.Value))
+				return nil, nil
+			})
+			if err != nil {
+				resp.Body.Result = "err"
+				return resp, errors.New("failed to set key")
+			}
+			resp.Body.Result = "ok"
+			return resp, nil
+		})
 
-    huma.Get(api, "/{key}", func(ctx context.Context, input *struct{
-      Key string `path:"key"`
-    }) (*GetKeyResponse, error) {
-      resp := &GetKeyResponse{}
-      ret, err := db.Transact(func(tr fdb.Transaction) (interface{}, error) {
-        return tr.Get(fdb.Key(input.Key)).MustGet(), nil
-      })
-      if err != nil {
-        return nil, errors.New("failed to get key")
-      }
-      resp.Body.Key = input.Key
-      resp.Body.Value = string(ret.([]byte))
-      return resp, nil
-    })
+		huma.Get(api, "/{key}", func(ctx context.Context, input *struct {
+			Key string `path:"key"`
+		}) (*GetKeyResponse, error) {
+			resp := &GetKeyResponse{}
+			ret, err := db.Transact(func(tr fdb.Transaction) (interface{}, error) {
+				return tr.Get(fdb.Key(input.Key)).MustGet(), nil
+			})
+			if err != nil {
+				return nil, errors.New("failed to get key")
+			}
+			resp.Body.Key = input.Key
+			resp.Body.Value = string(ret.([]byte))
+			return resp, nil
+		})
 
 		// Tell the CLI how to start your router.
 		hooks.OnStart(func() {
-      fmt.Println("Starting server on port", options.Port)
+			fmt.Println("Starting server on port", options.Port)
 			http.ListenAndServe(fmt.Sprintf(":%d", options.Port), router)
 		})
 	})
